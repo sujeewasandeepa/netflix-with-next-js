0236484b67db8a10c72662808e068a827d8a61e8
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2022-04-19 22:03:21 +0530

    create Header component and import to index file

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -1,19 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Let's build netflix
-
+      <Header />
       <main>
-        
+        {/* Banner */}
+        <section>
+
+        </section>
       </main>
+      {/* Modal */}
     </div>
   )
 }
